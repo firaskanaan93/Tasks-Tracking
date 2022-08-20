283600f2b944218f7da41b0e2fff3065356293a8
Firas Kanaan <firaskanaan93@gmail.com>
2022-08-20 15:02:14 +0200

    add relationships for task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-
+/**
+ * App\Models\Task
+ *
+ * @property integer $id
+ * @property integer $assigned_to_id
+ * @property integer $assigned_by_id
+ * @property string $title
+ * @property string $description
+ * @property User $admin
+ * @property User $user
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @mixin Builder
+ */
 class Task extends Model
 {
     protected $fillable = ['title','description','assigned_to_id','assigned_by_id'];
 
+    public function admin(){
+        return $this->belongsTo(User::class,'assigned_by_id');
+    }
+    public function user(){
+        return $this->belongsTo(User::class,'assigned_to_id');
+    }
 }
